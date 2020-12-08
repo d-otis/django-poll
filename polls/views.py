983c662c28c1ec4d7269e9a81ab2986c14eb1977
d-otis <dan@dan-foley.com>
2020-12-08 17:08:23 -0500

    refactored index()

--- polls/views.py
+++ polls/views.py
@@ -8,9 +8,19 @@ from .models import Question
 def index(request):
   # original example
   # return HttpResponse("Hello, world. You're @ the polls index!")
+  # ORIGINAL w/ Template
+  # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  # template = loader.get_template('polls/index.html')
+  # context = {
+  #   'latest_question_list': latest_question_list
+  # }
+  # return HttpResponse(template.render(context, request))
+  # REWRITTEN 
+  # since this is such a common pattern
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  output = ', '.join([q.question_text for q in latest_question_list])
-  return HttpResponse(output)
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
   return HttpResponse("You're looking @ question %s." % question_id)
