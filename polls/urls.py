0ea331e8d7c61d59d970ea0a3a43776ed1302026
d-otis <dan@dan-foley.com>
2020-12-08 16:30:51 -0500

    add routes for detail, results and vote

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,13 @@ urlpatterns = [
   # empty path means it uses the path from the root URLconf
   # if changed to 'foo' for example this view can be accessed at
   # /polls/foo
-  path('', views.index, name="index")
+  ##################################
+  # /polls/
+  path('', views.index, name="index"),
+  # /polls/5/
+  path('<int:question_id>/', views.detail, name="detail"),
+  # /polls/5/results/
+  path('<int:question_id>/results/', views.results, name="results"),
+  # /polls/5/vote/
+  path('<int:question_id>/vote/', views.vote, name="vote")
 ]
