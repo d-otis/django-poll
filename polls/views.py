96cc4821fd0f68e78e7d97cd4fac0b9c6711cb25
d-otis <dan@dan-foley.com>
2020-12-08 22:10:24 -0500

    import HttpResponseRedirect and reverse

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question
 # Create your views here.
