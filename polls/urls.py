90e0e04f15d255009c4b2714ec82aa8dcdb7d567
d-otis <dan@dan-foley.com>
2020-12-08 20:41:28 -0500

    add namespacing

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
   # empty path means it uses the path from the root URLconf
   # if changed to 'foo' for example this view can be accessed at
