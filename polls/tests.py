7b29c0b7f2954d75bf828f3991a05e0c7f3de429
d-otis <dan@dan-foley.com>
2020-12-12 15:43:54 -0500

    fix indentation error

--- polls/tests.py
+++ polls/tests.py
@@ -12,6 +12,6 @@ class QuestionModelTests(TestCase):
     was_published_recently() returns False for questions 
     whose pub_date is in the future
      """
-     time = timezone.now() + datetime.timedelta(days=30)
-     future_question = Question(pub_date=time)
-     self.asserts(future_question.was_published_recently(), False)
+    time = timezone.now() + datetime.timedelta(days=30)
+    future_question = Question(pub_date=time)
+    self.asserts(future_question.was_published_recently(), False)
