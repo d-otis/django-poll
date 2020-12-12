f5b77256a84dd4d3dea406557bf2c64a5289904e
d-otis <dan@dan-foley.com>
2020-12-12 14:50:44 -0500

    update URLconf to reflect usage of Django's generic views

--- polls/urls.py
+++ polls/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
   # /polls/foo
   ##################################
   # /polls/
-  path('', views.index, name="index"),
+  path('', views.IndexView.as_view(), name="index"),
   # /polls/5/
   # the 'name' value as called by the {% url %} template tag
-  path('<int:question_id>/', views.detail, name="detail"),
+  path('<int:pk>/', views.DetailView.as_view(), name="detail"),
   # /polls/5/results/
-  path('<int:question_id>/results/', views.results, name="results"),
+  path('<int:pk>/results/', views.ResultsView.as_view(), name="results"),
   # /polls/5/vote/
   path('<int:question_id>/vote/', views.vote, name="vote")
 ]
